886612d5eccb136c613357a0b1a0f58276b46c77
edwin-2024 <iamedwing15@gmail.com>
2026-02-12 18:52:38 +0530

    signup section code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -24,31 +24,30 @@ export const signup = async (req, res) => {
     const user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "Email already exists" });
 
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password,salt)
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       fullName,
       email,
-      password:hashedPassword
-    })
+      password: hashedPassword,
+    });
 
-    if(newUser) {
-      generateToken(newUser._id, res)
-      await newUser.save() 
+    if (newUser) {
+      const savedUser = await newUser.save();
+      generateToken(newUser._id, res);
 
       res.status(201).json({
         _id: newUser._id,
         fullName: newUser.fullName,
         email: newUser.email,
         profilePic: newUser.profilePic,
-      })
+      });
     } else {
-      res.status(400).json({message: "Invalid user data"})
+      res.status(400).json({ message: "Invalid user data" });
     }
-
   } catch (error) {
-    console.log("Error in signup controller: ", error)
-    res.status(500).json({message: "Internal server error"})
+    console.log("Error in signup controller: ", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
